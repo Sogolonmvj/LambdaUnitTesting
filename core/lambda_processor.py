7754ab69e68ea99402073876236a1bde782f78ae
Sogolon <sogolonmvj@yahoo.com>
2023-01-29 20:25:31 -0300

    feat: added custom exception!

--- core/lambda_processor.py
+++ core/lambda_processor.py
@@ -1,13 +1,14 @@
 import json
 
+from exception.invalid_operator_exception import InvalidOperatorException
 
 class Processor:
-    def __init__(self, first: int, second: int, operator: str):
+    def __init__(self, first: int, second: int, operator: str) -> None:
         self.first = first
         self.second = second
         self.operator = operator
 
-    def calculate(self):
+    def calculate(self) -> json:
         return {
             'statusCode': 200,
             'body': json.dumps(self.verify_operator())
@@ -24,4 +25,4 @@ class Processor:
             case "/":
                 return {'result': self.first / self.second}
             case default:
-                raise RuntimeError("Invalid operator!")
+                raise InvalidOperatorException("Invalid operator!")
